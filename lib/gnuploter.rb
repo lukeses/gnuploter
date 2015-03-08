2a1ce519f98c612eef342b793dc07d30f0e4050c
Lukasz Jankowski <lukaszpjankowski@gmail.com>
2015-03-08 19:47:03 +0100

    first version

--- lib/gnuploter.rb
+++ lib/gnuploter.rb
@@ -1,5 +1,24 @@
 require "gnuploter/version"
 
 module Gnuploter
-  # Your code goes here...
+  class Gnuplot
+    attr_accessor :title, :key, :samples, :style
+ # @parameters = %w(title key samples style)
+
+    def initialize
+      @plot = IO.popen("gnuplot", "w")
+      @parameters = %w(title key samples style)
+    end
+
+    def set_parameters
+      @parameters.each do |parameter|
+        @plot.puts 'set ' + parameter + ' ' +eval(parameter) unless defined? parameter
+        puts 'set ' + parameter + ' ' +eval(parameter) unless defined? parameter
+      end
+    end
+
+    def plot(function)
+      @plot.puts "plot " + function
+    end
+  end
 end
